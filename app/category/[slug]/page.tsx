786d6c120baed573c7de78c751312fdc77455d3e
Harshit <harshitobra19@gmail.com>
2025-11-28 01:13:41 +0530

    adding link to featured product

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import { use } from "react";
 import { notFound } from "next/navigation";
 import ProductDetail from "@/components/ProductDetail";
 import Footer from "@/components/Footer";
 import { productData } from "@/lib/productData";
 
-export default function CategoryPage({ params }: Readonly<{ params: { slug: string } }>) {
-  const product = productData[params.slug];
+export default function CategoryPage({ params }: Readonly<{ params: Promise<{ slug: string }> }>) {
+  const { slug } = use(params);
+  const product = productData[slug];
 
   if (!product) {
     notFound();
--- components/FeaturedProductsSection.tsx
+++ components/FeaturedProductsSection.tsx
@@ -8,6 +8,7 @@ interface Product {
   id: number;
   image: string;
   title: string;
+  slug: string;
 }
 
 const FeaturedProductsSection = () => {
@@ -17,283 +18,245 @@ const FeaturedProductsSection = () => {
   const products: Product[] = [
     {
       id: 1,
-      image:
-        "images/products/bolts/NYLON HEX BOLT.png",
+      image: "images/products/bolts/NYLON HEX BOLT.png",
       title: "Nylon Hex Bolt",
+      slug: "nylon-hex-bolt",
     },
     {
       id: 2,
-      image:
-        "images/productdata/bolts/Hex bolt ss.png",
-      title:
-        "Stainless Steel Hex Bolt",
+      image: "images/productdata/bolts/Hex bolt ss.png",
+      title: "Stainless Steel Hex Bolt",
+      slug: "hex-bolt",
     },
     {
       id: 3,
-      image:
-        "images/productdata/bolts/Flange bolt ss.png",
-      title:
-        "Flange Bolt",
+      image: "images/productdata/bolts/Flange bolt ss.png",
+      title: "Flange Bolt",
+      slug: "flange-bolt",
     },
     {
       id: 4,
-      image:
-        "images/productdata/bolts/wing bolt ss.png",
+      image: "images/productdata/bolts/wing bolt ss.png",
       title: "Wing Bolt",
+      slug: "wing-bolt",
     },
     {
       id: 5,
-      image:
-        "images/productdata/bolts/dome bolt ss.png",
+      image: "images/productdata/bolts/dome bolt ss.png",
       title: "Dome Bolt",
+      slug: "dome-bolt",
     },
     {
       id: 6,
-      image:
-        "images/productdata/bolts/carriage  bolt ss.png",
+      image: "images/productdata/bolts/carriage  bolt ss.png",
       title: "Carriage Bolt",
+      slug: "carriage-bolt",
     },
     {
       id: 7,
-      image:
-        "images/productdata/screws/allen screws/Allen bolt ss.png",
+      image: "images/productdata/screws/allen screws/Allen bolt ss.png",
       title: "SHCS Machine Screw",
+      slug: "shcs-machine-screws",
     },
     {
       id: 8,
-      image:
-        "images/productdata/screws/allen screws/Allen csk bolt ms.png",
+      image: "images/productdata/screws/allen screws/Allen csk bolt ms.png",
       title: "Allen CSK Machine Screw",
+      slug: "csk-machine-screws",
     },
     {
       id: 9,
-      image:
-        "images/productdata/screws/allen screws/Button head bolt ms.png",
+      image: "images/productdata/screws/allen screws/Button head bolt ms.png",
       title: "Button Head Machine Screw",
+      slug: "bhcs-machine-screws",
     },
     {
       id: 10,
-      image:
-        "images/productdata/screws/allen screws/Grub ss.png",
+      image: "images/productdata/screws/allen screws/Grub ss.png",
       title: "GRUB Screw",
+      slug: "grub-screws",
     },
-
-    
-  {
+    {
       id: 11,
-      image:
-        "images/productdata/screws/allen screws/JCB ss.png",
+      image: "images/productdata/screws/allen screws/JCB ss.png",
       title: "JCB Screw",
+      slug: "jcb-screws",
     },
-
     {
       id: 12,
-      image:
-        "images/productdata/screws/allen screws/Pan Torx Machine Screw ss.png",
+      image: "images/productdata/screws/allen screws/Pan Torx Machine Screw ss.png",
       title: "Pan Torx Machine Screw",
+      slug: "pan-torx",
     },
-
     {
       id: 13,
-      image:
-        "images/productdata/screws/self tapping screws/STS CSK (+) ss.png",
+      image: "images/productdata/screws/self tapping screws/STS CSK (+) ss.png",
       title: "STS CSK (+)",
+      slug: "csk-phillips-sts",
     },
-
     {
       id: 14,
-      image:
-        "images/productdata/screws/self tapping screws/STS Pan (+) ss.png",
+      image: "images/productdata/screws/self tapping screws/STS Pan (+) ss.png",
       title: "STS PAN (+)",
+      slug: "pan-phillips-sts",
     },
-
     {
       id: 15,
-      image:
-        "images/productdata/screws/self tapping screws/STS CSK B Type ss.png",
+      image: "images/productdata/screws/self tapping screws/STS CSK B Type ss.png",
       title: "STS CSK B Type",
+      slug: "csk-b-type-sts",
     },
-
     {
       id: 16,
-      image:
-        "images/productdata/screws/self tapping screws/STS Pan B type ss.png",
+      image: "images/productdata/screws/self tapping screws/STS Pan B type ss.png",
       title: "STS PAN B Type",
+      slug: "pan-b-type-sts",
     },
-
-     {
+    {
       id: 17,
-      image:
-        "images/productdata/screws/self tapping screws/STS WW Pan (+) ss.png",
+      image: "images/productdata/screws/self tapping screws/STS WW Pan (+) ss.png",
       title: "STS WW Pan (+)",
+      slug: "ww-pan-phillips-sts",
     },
-     
     {
       id: 18,
-      image:
-        "images/productdata/screws/self tapping screws/Pan Torx STS ss.png",
+      image: "images/productdata/screws/self tapping screws/Pan Torx STS ss.png",
       title: "Pan Torx STS",
+      slug: "pan-torx-sts",
     },
-     
     {
       id: 19,
-      image:
-        "images/productdata/screws/self tapping screws/CSK Torx STS ss.png",
+      image: "images/productdata/screws/self tapping screws/CSK Torx STS ss.png",
       title: "CSK Torx STS",
+      slug: "csk-torx-sts",
     },
-
     {
       id: 20,
-      image:
-        "images/productdata/screws/self drilling screws/Pan Self-Drilling Screw ss.png",
+      image: "images/productdata/screws/self drilling screws/Pan Self-Drilling Screw ss.png",
       title: "Pan Self-Drilling Screws",
+      slug: "pan-sds",
     },
-
-     {
+    {
       id: 21,
-      image:
-        "images/productdata/screws/self drilling screws/Hex Self-Drilling Screw ss.png",
+      image: "images/productdata/screws/self drilling screws/Hex Self-Drilling Screw ss.png",
       title: "Hex Self-Drilling Screws",
+      slug: "hex-sds",
     },
-
     {
       id: 22,
-      image:
-        "images/productdata/screws/self drilling screws/CSK Self-Drilling Screw ss.png",
+      image: "images/productdata/screws/self drilling screws/CSK Self-Drilling Screw ss.png",
       title: "CSK Self-Drilling Screws",
+      slug: "csk-sds",
     },
-
     {
       id: 23,
-      image:
-        "images/productdata/nuts/Wafer Head Self-Drilling Screw ss.png",
+      image: "images/productdata/nuts/Wafer Head Self-Drilling Screw ss.png",
       title: "Truss Phillips Head Self-Drilling Screws",
+      slug: "truss-phillips",
     },
-
     {
       id: 24,
-      image:
-        "images/productdata/screws/phillip machine screws/Pan head Phillips nylon.png",
+      image: "images/productdata/screws/phillip machine screws/Pan head Phillips nylon.png",
       title: "Nylon Pan Head Phillips",
+      slug: "pan-head-phillips",
     },
-
     {
       id: 25,
-      image:
-        "images/productdata/screws/phillip machine screws/CSK Head phillips nylon.png",
+      image: "images/productdata/screws/phillip machine screws/CSK Head phillips nylon.png",
       title: "Nylon CSK Head Phillips",
+      slug: "csk-head-phillips",
     },
-
     {
       id: 26,
-      image:
-        "images/productdata/spacers/Nylon ET Spacer.png",
+      image: "images/productdata/spacers/Nylon ET Spacer.png",
       title: "Nylon ET Spacer",
+      slug: "nylon-et-spacer",
     },
-
     {
       id: 27,
-      image:
-        "images/productdata/spacers/Nylon Hex Spacer.png",
+      image: "images/productdata/spacers/Nylon Hex Spacer.png",
       title: "Nylon Hex Spacer",
+      slug: "nylon-hex-spacer",
     },
-
     {
       id: 28,
-      image:
-        "images/productdata/spacers/Nylon Round Plain Spacer.png",
+      image: "images/productdata/spacers/Nylon Round Plain Spacer.png",
       title: "Nylon Round Plain Spacer",
+      slug: "nylon-round-plain-spacer",
     },
-
     {
       id: 29,
-      image:
-        "images/productdata/nuts/Hex Nut ss.png",
+      image: "images/productdata/nuts/Hex Nut ss.png",
       title: "Stainless Steel Hex Nut",
+      slug: "hex-nut",
     },
-
     {
       id: 30,
-      image:
-        "images/productdata/nuts/Hex Nut Nylon.png",
+      image: "images/productdata/nuts/Hex Nut Nylon.png",
       title: "Nylon Hex Nut",
+      slug: "hex-nut",
     },
-
     {
       id: 31,
-      image:
-        "images/productdata/nuts/Nyloc Nut ss.png",
+      image: "images/productdata/nuts/Nyloc Nut ss.png",
       title: "Nyloc Nut",
+      slug: "nyloc-nut",
     },
-
     {
       id: 32,
-      image:
-        "images/productdata/nuts/Flange Nut ss.png",
+      image: "images/productdata/nuts/Flange Nut ss.png",
       title: "Flange Nut",
+      slug: "flange-nut",
     },
-
     {
       id: 33,
-      image:
-        "images/productdata/washers/Spring Washer ss.png",
+      image: "images/productdata/washers/Spring Washer ss.png",
       title: "Spring Washer",
+      slug: "spring-washer",
     },
-
     {
       id: 34,
-      image:
-        "images/productdata/washers/Plain Washer.png",
+      image: "images/productdata/washers/Plain Washer.png",
       title: "Plain Washer",
+      slug: "plain-washer",
     },
-
- 
-
     {
       id: 35,
-      image:
-        "images/productdata/bolts/Blind Stand Off ss.png",
+      image: "images/productdata/bolts/Blind Stand Off ss.png",
       title: "Blind Stand Off",
+      slug: "blind-stand-off",
     },
-
     {
       id: 36,
-      image:
-        "images/productdata/bolts/Through Hole Stand Off ss.png",
+      image: "images/productdata/bolts/Through Hole Stand Off ss.png",
       title: "Through Hole Stand Off",
+      slug: "through-hole-stand-off",
     },
-
     {
       id: 37,
-      image:
-        "images/productdata/rivets&dowels/Blind POP Rivet ss.png",
+      image: "images/productdata/rivets&dowels/Blind POP Rivet ss.png",
       title: "Blind POP Rivet",
+      slug: "blind-pop-rivet",
     },
-
     {
       id: 38,
-      image:
-        "images/productdata/anchors/Wedge Anchor ss.png",
+      image: "images/productdata/anchors/Wedge Anchor ss.png",
       title: "Wedge Anchor",
+      slug: "wedge-anchor",
     },
-
     {
       id: 39,
-      image:
-        "images/productdata/anchors/Pin-Type Anchor ss.png",
+      image: "images/productdata/anchors/Pin-Type Anchor ss.png",
       title: "Pin-Type Anchor",
+      slug: "pin-type-anchor",
     },
-
     {
       id: 40,
-      image:
-        "images/productdata/screws/allen screws/CSK Torx Machine Screw ss.png",
+      image: "images/productdata/screws/allen screws/CSK Torx Machine Screw ss.png",
       title: "CSK Torx Machine Screw",
+      slug: "csk-torx",
     },
-
-
-    
-];
+  ];
 
   // Group products into slides (8 products per slide - 2 rows of 4)
   const productsPerSlide = 8;
@@ -361,7 +324,7 @@ const FeaturedProductsSection = () => {
                   {slideProducts.map((product) => (
                     <Link
                       key={product.id}
-                      href="/products"
+                      href={`/category/${product.slug}`}
                       className="flex flex-col items-center group"
                     >
                       {/* Card container for image only (dark background to match grid) */}
